f53ef5bcd7016a63c9882d8bf723db6bc278fb03
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2020-08-17 17:49:19 +0700

    my covid-19 tracker

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Installation
+
+1. Run `npm install`.
+
+2. Run `npm run dev` to start the frontend
+
+## Build the app
+
+1. Run `npm run build`.
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/build"),
+        filename: "app_bundle.js",
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                ],
+                include: /\.module\.css$/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /\.module\.css$/,
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+        }),
+    ],
+};
